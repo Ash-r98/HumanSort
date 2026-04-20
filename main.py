c2d838b3c15b74bc3021e99e26310cd69886884d
Ash-r98 <duncanjgmcintosh@gmail.com>
2026-04-20 10:26:18 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from random import randint
+from datetime import datetime, timedelta
+
+# Range for length of data
+itemnumlower = 5
+itemnumhigher = 10
+# Range for size of items
+itemrangelower = 1
+itemrangehigher = 99
+
+data = []
+for i in range(randint(itemnumlower, itemnumhigher)):
+    data.append(randint(itemrangelower, itemrangehigher))
+
+print(data)
+print()
+print("Sorted List:")
+
+humandata = []
+for i in range(len(data)):
+    while True:
+        try:
+            newitem = int(input(f"Item {i+1}/{len(data)}:\n"))
+            humandata.append(newitem)
+            break
+        except:
+            print("Invalid input")
+print()
+print(f"Original data:\n{data}\n")
+print(f"Hopefully sorted data:\n{humandata}")
